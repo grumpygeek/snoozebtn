47016209f732d305cfbd612d833cf1ebe2744fbe
grumpygeek <grumpygeek@users.noreply.github.com>
2016-10-17 07:10:20 -0400

    Create code1.js

---
+++ code1.js
@@ -0,0 +1,8 @@
+/*some basic functions I may use,
+ or code for testing (will be marked)*/
+
+//not particularly useful as is
+function pinsrt(pID, txInsert){
+  document.getElementById(pID) = txInsert;
+  return 1;
+ }
